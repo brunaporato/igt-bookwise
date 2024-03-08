7980a275c02767d45686d269c24146e49d02b3c1
Bruna <brunaporato@gmail.com>
2024-03-08 16:34:27 -0300

    feat new api fetch

--- src/pages/api/books/index.api.ts
+++ src/pages/api/books/index.api.ts
@@ -7,7 +7,23 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const books = await prisma.book.findMany()
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+  const categoryId = req.query.category as string
+
+  const books = await prisma.book.findMany({
+    where: {
+      categories: {
+        some: {
+          categoryId,
+        },
+      },
+    },
+    include: {
+      ratings: true,
+    },
+  })
 
   return res.json({ books })
 }
