20e3d85baa94038c7f1534f7dea0f3a035dcbe72
Bruna <brunaporato@gmail.com>
2024-03-08 16:34:02 -0300

    add queryprovider

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,6 +3,8 @@ import { AppProps } from 'next/app'
 // eslint-disable-next-line camelcase
 import { Nunito_Sans } from 'next/font/google'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 export const nunito = Nunito_Sans({
   subsets: ['latin'],
@@ -15,10 +17,12 @@ export default function MyApp({
   globalStyles()
 
   return (
-    <div className={`${nunito.className}`}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className={`${nunito.className}`}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </div>
+    </QueryClientProvider>
   )
 }
