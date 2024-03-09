4ae60d2ad540752ebea866e9921fbecf64fbd7fd
Bruna <brunaporato@gmail.com>
2024-03-09 17:14:08 -0300

    add possible null avatar

--- src/pages/components/Avatar/index.tsx
+++ src/pages/components/Avatar/index.tsx
@@ -1,7 +1,7 @@
 import { AvatarContainer } from './styles'
 
 interface AvatarProps {
-  avatar: string
+  avatar: string | null | undefined
   variant?: 'card' | 'profile'
   onClick?: () => void
 }
@@ -10,7 +10,10 @@ export function Avatar({ avatar, variant, onClick }: AvatarProps) {
   return (
     <AvatarContainer
       onClick={onClick}
-      src={avatar}
+      src={
+        avatar ??
+        'https://static.vecteezy.com/system/resources/previews/005/005/788/original/user-icon-in-trendy-flat-style-isolated-on-grey-background-user-symbol-for-your-web-site-design-logo-app-ui-illustration-eps10-free-vector.jpg'
+      }
       alt=""
       width={32}
       height={32}
