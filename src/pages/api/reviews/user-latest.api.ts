c669668f9b72f2c3ca1f12ddb88c8ad2b839d0f6
Bruna <brunaporato@gmail.com>
2024-03-09 17:13:55 -0300

    add new api routes

---
+++ src/pages/api/reviews/user-latest.api.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../auth/[...nextauth].api'
+import { prisma } from '@/lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const userEmail = String(session?.user?.email)
+  const user = await prisma.user.findUnique({
+    where: {
+      email: userEmail,
+    },
+  })
+
+  if (!user) {
+    return res.status(404).json({
+      error: 'User not found',
+    })
+  }
+
+  const userId = user.id
+
+  const latestUserReview = await prisma.rating.findFirst({
+    where: {
+      user_id: userId,
+    },
+    orderBy: {
+      created_at: 'desc',
+    },
+    include: {
+      book: true,
+    },
+  })
+
+  return res.json({ review: { ...latestUserReview, user } })
+}
